35e3722f3ec45144c2d25210213dde87e87bd830
Mikel Rouco <mrouco001@ikasle.ehu.eus>
2017-10-24 19:18:40 +0200

    Add Normal class and added methods to both classes

--- superconductivity/__init__.py
+++ superconductivity/__init__.py
@@ -2,5 +2,5 @@ import numpy as _np
 import scipy as _scipy
 
 from ._selfconsistency import sc_delta, template_bcs_th
-from ._superconductor import Superconductor
+from ._superconductor import Superconductor, sampler_1d
 
